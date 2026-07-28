9194ce934affbadb7f179164bfa7b7a198c1c510
Tam Nguyen <tam.nguyen@mercatus.com>
2026-07-29 00:34:47 +0700

    chore: add Capacitor core and Android platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ndtcore.pos',
+  appName: 'NDTCore POS',
+  webDir: 'dist'
+};
+
+export default config;
